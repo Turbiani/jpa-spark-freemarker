3b46cbb1194df67f6061c2c6f3491af57cd6a16c
turbiani <leo.turbiani@gmail.com>
2015-07-07 01:18:54 -0300

    Tentando criar uma suite de testes de sistema, para que start o server antes de todos os testes de sistema...ainda não está 100%, porém agora já está compilando e rodando o primeiro teste com Seleniun

--- src/test/java/br/com/estudos/jpasparkfreemarker/system/ContaSystemTest.java
+++ src/test/java/br/com/estudos/jpasparkfreemarker/system/ContaSystemTest.java
@@ -1,17 +1,32 @@
 package br.com.estudos.jpasparkfreemarker.system;
 
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import br.com.estudos.jpasparkfreemarker.controller.MainController;
+
 public class ContaSystemTest {
 	private WebDriver driver;	
+	
+	@BeforeClass
+	public static void inicializaServer(){	
+		String[] params = null;
+		MainController.main(params);
+	}
+	
+	@AfterClass
+	public static void desligaServer(){
+		Runtime.getRuntime().exit(1);
+	}
 	
 	@Before
 	public void inicializa(){	
@@ -27,17 +42,17 @@ public class ContaSystemTest {
 		WebElement agencia = driver.findElement(By.name("agencia"));
 		WebElement banco = driver.findElement(By.name("banco"));
 		
-		titular.sendKeys("Teste Automatizado pelo amor");
-		numero.sendKeys("1990");
-		agencia.sendKeys("Av Edigar Faco");
-		banco.sendKeys("Bradesco");
+		titular.sendKeys("Teste Automatizado pelo junit");
+		numero.sendKeys("1993");
+		agencia.sendKeys("Av Francisco Morato");
+		banco.sendKeys("City bank");
 		
 		titular.submit();
 		
 		//INDO PARA PAGINA DE LISTAGEM DAS CONTAS
 		driver.get("http://localhost:8080/conta/lista");
 		
-		assertTrue(driver.getPageSource().contains("Teste Automatizado"));
-		assertTrue(driver.getPageSource().contains("1991"));
+		assertTrue(driver.getPageSource().contains("Teste Automatizado pelo junit"));
+		assertTrue(driver.getPageSource().contains("1993"));
 	}
 }
---
+++ src/test/java/br/com/estudos/jpasparkfreemarker/system/suite/CrudSuiteTest.java
@@ -0,0 +1,29 @@
+package br.com.estudos.jpasparkfreemarker.system.suite;
+
+import java.io.IOException;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import br.com.estudos.jpasparkfreemarker.controller.MainController;
+import br.com.estudos.jpasparkfreemarker.system.ContaSystemTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({ContaSystemTest.class})
+public class CrudSuiteTest {
+	
+	/*@BeforeClass
+	public static void inicializaServer() {
+		String[] params = null; 
+		MainController.main(params);
+	}
+	
+	@AfterClass
+	public static void desligaServer(){
+		Runtime.getRuntime().exit(1);
+	}*/
+	
+}
